82b671242367671aa275a758f6f1f4374d52415f
flukehermit <zayed.humayun@g.bracu.ac.bd>
2022-10-10 08:51:56 +0600

    Added top games

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,22 @@
 import SearchResults from '../components/SearchResults';
 import SearchBar from '../components/SearchBar';
 import SteamUsersOnline from '../components/SteamUsersOnline';
+import TopGames from '../components/TopGames';
 
 const Home = () => {
   return (
     <div className='bg-[#0c1820] h-screen'>
+      <SteamUsersOnline />
       <nav>
         <img
           className='w-40'
           src={`${process.env.PUBLIC_URL}/logo_steam.png`}
+          alt='banner'
         />
         <SearchBar />
       </nav>
+      <TopGames />
 
-      <SearchResults />
       <SteamUsersOnline />
     </div>
   );
